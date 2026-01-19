3a43312e83521889b3482fcdf19185611a291556
Michael Adler <michael.adler@siemens.com>
2026-01-19 11:19:34 +0100

    feat: update to Debian trixie
    Signed-off-by: Michael Adler <michael.adler@siemens.com>

--- scripts/start-margo-device.sh
+++ scripts/start-margo-device.sh
@@ -15,9 +15,9 @@ fi
 case $MACHINE in
 qemu-amd64)
     IMAGE_DIR=${KAS_WORK_DIR:-.}/build/tmp/deploy/images/qemu-amd64
-    KERNEL_FILE=$IMAGE_DIR/margo-device-image-cip-core-bookworm-qemu-amd64-vmlinuz
-    INITRD_FILE=$IMAGE_DIR/margo-device-image-cip-core-bookworm-qemu-amd64-initrd.img
-    IMAGE_FILE=$IMAGE_DIR/margo-device-image-cip-core-bookworm-qemu-amd64.ext4
+    KERNEL_FILE=$IMAGE_DIR/margo-device-image-cip-core-trixie-qemu-amd64-vmlinuz
+    INITRD_FILE=$IMAGE_DIR/margo-device-image-cip-core-trixie-qemu-amd64-initrd.img
+    IMAGE_FILE=$IMAGE_DIR/margo-device-image-cip-core-trixie-qemu-amd64.ext4
     KERNEL_CMDLINE="console=ttyS0,115200 root=/dev/sda rw"
 
     exec qemu-system-x86_64 -M pc -enable-kvm -smp 4 -m 2G \
@@ -33,9 +33,9 @@ qemu-amd64)
 
 qemu-arm64)
     IMAGE_DIR=${KAS_WORK_DIR:-.}/build/tmp/deploy/images/qemu-arm64
-    KERNEL_FILE=$IMAGE_DIR/margo-device-image-cip-core-bookworm-qemu-arm64-vmlinux
-    INITRD_FILE=$IMAGE_DIR/margo-device-image-cip-core-bookworm-qemu-arm64-initrd.img
-    IMAGE_FILE=$IMAGE_DIR/margo-device-image-cip-core-bookworm-qemu-arm64.ext4
+    KERNEL_FILE=$IMAGE_DIR/margo-device-image-cip-core-trixie-qemu-arm64-vmlinux
+    INITRD_FILE=$IMAGE_DIR/margo-device-image-cip-core-trixie-qemu-arm64-initrd.img
+    IMAGE_FILE=$IMAGE_DIR/margo-device-image-cip-core-trixie-qemu-arm64.ext4
     KERNEL_CMDLINE="console=ttyAMA0,115200 root=/dev/vda rw"
 
     exec qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 4 -m 2G \
